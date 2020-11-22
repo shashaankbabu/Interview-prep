1deae3fdb2bc3c560eb23c177aabcdf3a228b07e
Shashaank <shashaankramesh@gmail.com>
2020-11-22 10:30:22 +0530

    Code Commit

---
+++ src/LeetCode/PathSum.java
@@ -0,0 +1,9 @@
+package LeetCode;
+
+class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        else if(root.left == null && root.right == null && sum - root.val == 0) return true;
+        else return hasPathSum(root.left, sum-root.val) ||  hasPathSum(root.right, sum-root.val);
+    }
+}
---
+++ src/LeetCode/PathSumII.java
@@ -0,0 +1,24 @@
+package LeetCode;
+import java.util.*;
+
+class PathSumII {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> paths = new ArrayList<>();
+
+        findPaths(root, sum, new ArrayList<Integer>(), paths);
+
+        return paths;
+    }
+
+    private void findPaths(TreeNode root, int sum, List<Integer> current, List<List<Integer>> paths) {
+        if(root == null) return;
+        current.add(root.val);
+        if(root.val == sum && root.left == null && root.right == null) {
+            paths.add(current);
+            return;
+        }
+
+        findPaths(root.left, sum-root.val, new ArrayList<Integer>(current), paths);
+        findPaths(root.right, sum-root.val, new ArrayList<Integer>(current), paths);
+    }
+}
