c8013cdfc1949093947a2df166839b67d672a383
Shashaank <shashaankramesh@gmail.com>
2020-12-03 13:48:37 +0530

    Code Commit

---
+++ src/LeetCode/RemoveDuplicateFromSortedList.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+class RemoveDuplicateFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while(current != null && current.next != null) {
+            if(current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
