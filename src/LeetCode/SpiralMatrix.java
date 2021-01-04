159ec15dc537ed4e27aaf726b56f7a34893bf2d7
Shashaank <shashaankramesh@gmail.com>
2021-01-04 17:24:50 +0530

    Code Commit

---
+++ src/LeetCode/SpiralMatrix.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+import java.util.List;
+import java.util.ArrayList;
+
+class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> nums = new ArrayList<>();
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int size = matrix.length * matrix[0].length;
+
+        while(nums.size() < size) {
+            for(int i=left; i<=right && nums.size()<size; i++) {
+                nums.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top; i<=bottom && nums.size()<size; i++) {
+                nums.add(matrix[i][right]);
+            }
+            right--;
+            for(int i=right; i>=left && nums.size()<size; i--) {
+                nums.add(matrix[bottom][i]);
+            }
+            bottom--;
+            for(int i=bottom; i>=top && nums.size()<size; i--) {
+                nums.add(matrix[i][left]);
+            }
+            left++;
+        }
+
+        return nums;
+    }
+}
