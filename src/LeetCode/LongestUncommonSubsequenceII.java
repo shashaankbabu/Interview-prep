771ff94a59d4ee957d7d08acbb82b6943fa6b1fc
Shashaank <shashaankramesh@gmail.com>
2021-01-25 18:09:42 +0530

    Code Commit

---
+++ src/LeetCode/LongestUncommonSubsequenceII.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+class LongestUncommonSubsequenceII {
+    public int findLUSlength(String[] strs) {
+        int result = -1;
+        for(int i = 0; i < strs.length; i++){
+            boolean isSubsequence = false;
+            for(int j = 0; j < strs.length; j++){
+                if(i != j){
+                    if(helper(strs[i], strs[j])){
+                        isSubsequence = true;
+                        break;
+                    }
+                }
+            }
+            if(!isSubsequence){
+                result = Math.max(result, strs[i].length());
+            }
+        }
+        return result;
+    }
+
+    private boolean helper(String a, String b){
+        if(a.length() > b.length()){
+            return false;
+        }
+        char[] arr1 = a.toCharArray();
+        char[] arr2 = b.toCharArray();
+        int i = 0;
+        int j = 0;
+        while(j < arr2.length){
+            if(i < arr1.length && arr2[j] == arr1[i]){
+                i++;
+            }
+
+            j++;
+        }
+        if(i == arr1.length){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
