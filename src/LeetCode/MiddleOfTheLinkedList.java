8d8dbe317c7b8ff9cb4df6d4e054873940bc2e5e
Shashaank <shashaankramesh@gmail.com>
2021-02-07 18:49:21 +0530

    Code Commit

---
+++ src/LeetCode/MiddleOfTheLinkedList.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+class MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
